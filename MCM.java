73683f8cd0338b11fe23054d5ecc7c15cf6fe8dc
Juan <juanyoung41@gmail.com>
2022-07-12 22:31:12 +0700

    Merapikan

--- MCM.java
+++ MCM.java
@@ -36,20 +36,20 @@ public class MCM{
     }
     public static void main(String[] args) {
         Scanner br = new Scanner(System.in);
-            System.out.println("Masukan Array p[] (Misal User Input = 5 3 8 9 19)");
-            String input = br.nextLine();
-            String[] splited = input.split(" ");
-            int[] values = Stream.of(splited).mapToInt(Integer::parseInt).toArray();
-            ArrayList <int[][]> arr2 = MatrixChainOrder(values);
-            int[][] a = arr2.get(0);
-            int[][] b = arr2.get(1);
+        System.out.println("Masukan Array p[] (Misal User Input = 5 3 8 9 19)");
+        String input = br.nextLine();
+        String[] splited = input.split(" ");
+        int[] values = Stream.of(splited).mapToInt(Integer::parseInt).toArray();
+        ArrayList <int[][]> arr2 = MatrixChainOrder(values);
+        int[][] a = arr2.get(0);
+        int[][] b = arr2.get(1);
 
-            System.out.println("\nMatrix Jumlah Perkalian : ");
+        System.out.println("\nMatrix Jumlah Perkalian : ");
         for(int i = 1 ; i < a.length ; i++){
             for(int j = 1; j < a[i].length ; j++){
                 System.out.print(a[i][j]+"\t");
             }
-            System.out.println();
+        System.out.println();
         }
 
         
