6092361c4c57b6aef8a8ca6ef3d23f308143d72d
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2022-07-15 10:17:13 +0700

    minor change dan formatting kode

--- LCS.java
+++ LCS.java
@@ -1,30 +1,30 @@
 import java.util.*;
 
-public class LCS{
+public class LCS {
 
-    public static void LCS_Lenght(String InputX[], String InputY[]){
+    public static ArrayList LCS_Lenght(String InputX[], String InputY[]) {
         int m = InputX.length;
         int n = InputY.length;
-        int c[][] = new int[m+1][n+1];
-        String b[][] = new String[m+1][n+1];
-        
-        for(int i = 1; i <= m;i++){
+        int c[][] = new int[m + 1][n + 1];
+        String b[][] = new String[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
             c[i][0] = 0;
         }
-        for(int j = 1; j <= n;j++){
+        for (int j = 1; j <= n; j++) {
             c[0][j] = 0;
         }
 
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
-                if(InputX[i-1].equals(InputY[j-1])){
-                    c[i][j] = c[i-1][j-1]+1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (InputX[i - 1].equals(InputY[j - 1])) {
+                    c[i][j] = c[i - 1][j - 1] + 1;
                     b[i][j] = "\\";
-                }else if(c[i-1][j]>= c[i][j-1]){
-                    c[i][j] =  c[i-1][j];
+                } else if (c[i - 1][j] >= c[i][j - 1]) {
+                    c[i][j] = c[i - 1][j];
                     b[i][j] = "^";
-                }else{
-                    c[i][j] = c [i][j-1];
+                } else {
+                    c[i][j] = c[i][j - 1];
                     b[i][j] = "<-";
                 }
             }
@@ -32,46 +32,55 @@ public class LCS{
 
         System.out.println("=============================================================================");
         System.out.println("Tabel C : ");
-        for(int i = 0 ; i < c.length ; i++){
-            for(int j = 0; j < c[i].length ; j++){
-                System.out.print(c[i][j]+"\t");
+
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < c[i].length; j++) {
+                System.out.print(c[i][j] + "\t");
             }
             System.out.println();
         }
-        
+
         System.out.println("=============================================================================");
         System.out.println("Tabel B (Arah) : ");
-        for(int i = 0 ; i < b.length; i++){
-            for(int j = 0; j < b[i].length ; j++){
-                if(i==0 && j==0){
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                if (i == 0 && j == 0) {
                     System.out.print("\t");
-                }else if (b[i][j] == null) {
-                    System.out.print    ("0\t");
-                }else{
-                    System.out.print(b[i][j]+"\t");
+                } else if (b[i][j] == null) {
+                    System.out.print("0\t");
+                } else {
+                    System.out.print(b[i][j] + "\t");
                 }
             }
-            System.out.println();    
+            System.out.println();
         }
-    }
-    public static void main(String[] args) {
-        String X[] = {"A","B","C","B","D","A","B"};
-        String Y[] = {"B","D","C","A","B","A"};
-        LCS_Lenght(X, Y);
-        
+        System.out.println("=============================================================================");
+        ArrayList<String[][]> arrs = new ArrayList<>();
+        arrs.add(b);
+        return arrs;
     }
 
-    public static void Print_LCS(String[][] b, String[] X, int i, int j){
-        if( i==0 || j==0){
+}
+
+
+    public static void main(String[] args) {
+        String X[] = {"A", "B", "C", "B", "D", "A", "B"};
+        String Y[] = {"B", "D", "C", "A", "B", "A"};
+        LCS_Lenght(X, Y);
+
+    }
+
+    public static void Print_LCS(String[][] b, String[] X, int i, int j) {
+        if (i == 0 || j == 0) {
             return;
         }
-        if(b[i][j]=="\\"){
-            Print_LCS(b, X, i-1, j-1);
-            System.out.print(X[i-1]);
-        }else if(b[i][j]=="^"){
-            Print_LCS(b, X, i-1, j);
-        }else{
-            Print_LCS(b, X, i, j-1);
+        if (b[i][j] == "\\") {
+            Print_LCS(b, X, i - 1, j - 1);
+            System.out.print(X[i - 1]);
+        } else if (b[i][j] == "^") {
+            Print_LCS(b, X, i - 1, j);
+        } else {
+            Print_LCS(b, X, i, j - 1);
         }
     }
 }
